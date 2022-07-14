d4b76f415ed6db78944200027f773fa649ffe5cb
Matthew Parker <mattparker124@gmail.com>
2022-07-14 17:57:08 -0500

    added stripe in isolation

--- server/server.js
+++ server/server.js
@@ -20,6 +20,40 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = ([
+  [1, { priceInCents: 10000, name: 'Testing Item 1'}],
+  [2, { priceInCents: 20000, name: 'Testing Item 2'}],
+])
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: req.body.items.map(item => {
+        const storeItem = storeItems.get(item.id)
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: storeItem.name
+            },
+            unit_amount: storeItem.priceInCents
+          },
+          quantity: item.quantity
+        }
+      }),
+      success_url: `${process.env.SERVER_URL}/success`,
+      cancel_url: `${process.env.SERVER_URL}/cancel`
+    })
+    res.json({ url: session.url })
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+})
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
