91f73051f11930711b2343f0a2b89a705f9ce1be
Nosferatu <davonb113@gmail.com>
2022-07-14 16:31:46 -0500

    stache:

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -2,8 +2,16 @@ import './index.scss';
 
 const User = () => {
     return (
-        <div>
-            User
+        <div className="container">
+            <h1>User's Pet</h1>
+
+            <div className="pet-container">
+                <div>
+                    Filler
+                </div>
+            </div>
+
+            <h2>Pet Name</h2>
         </div>
     )
 }
