6fc58f8c86a5415dfe8e3479cb37f430a590aa64
Nosferatu <davonb113@gmail.com>
2022-07-16 19:05:07 -0500

    added store styling

--- client/src/components/Store/index.js
+++ client/src/components/Store/index.js
@@ -1,9 +1,30 @@
 import './index.scss';
-
+import Merchant from '../../images/merchant.png'
 const Store = () => {
     return (
-        <div>
-            Store
+        <div className='container'>
+            <div className='merchant-container'>
+                <img className='merchant' src={Merchant} />
+                <p>FILLER IMAGE</p>
+            </div>
+            <div className='options'>
+                <ul>
+                    <li>
+                        Candy
+                    </li>
+                    <li>
+                        Coins
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Hat 1
+                    </li>
+                    <li>
+                        Hat 2
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
