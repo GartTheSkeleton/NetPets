7985d0f22cc03c789134d4587f641ef2024d572b
Gart <gmhobson@gmail.com>
2022-07-18 13:36:09 -0500

    dropdown passes value to function

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,19 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME} from '../utils/queries';
+
 
 
 const Profile = () => {
     const {data} = useQuery(QUERY_ME)
     console.log(data?.me)
     const activePet = data?.me.pets
+
+    const Func = async () =>{
+        const speciesDropdown = document.getElementById("speciesSelect")
+        console.log(speciesDropdown.value)
+    }
+
     if (activePet === null)
         {
             return(
                 <div>
-                    <div>What Pet Would You Like?</div>
-                    <button>Pet 1</button>
-                    <button>Pet 2</button>
-                    <button>Pet 3</button>
+                    <form className='form'>
+                        <h2>Looks' like you dont have a pet yet.</h2>
+                        <p>Select Your Species</p>
+                        <select id="speciesSelect" onChange={(e) => Func()}>
+                            <option value="species 1">species 1</option>
+                            <option value="species 2">species 2</option>
+                            <option value="species 3">species 3</option>
+                        </select>
+                        <p>Name Your Pet</p>
+                        <textarea defaultValue={"Nickname"}></textarea>
+                        <p></p>
+                        <button type='submit'>Submit</button>
+                    </form>
                 </div>
                 
             )
