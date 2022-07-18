f25a600e2a6ae6ed91117fe340f8a51a937a9022
Nosferatu <davonb113@gmail.com>
2022-07-18 15:58:29 -0500

    styling done to store buttons

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -1,12 +1,29 @@
 import { useQuery } from '@apollo/client';
 import { QUERY_ME} from '../utils/queries';
+import Merchant from '../images/merchant.png';
+import '../components/Layout/index.scss';
 
 const Store = () => {
     const {data} = useQuery(QUERY_ME)
     console.log(data?.me)
     return (
-        <div>
-            Store
+        <div className='container'>
+            <div className='merchant-container'>
+                <img className='merchant' src={Merchant} />
+                <p>FILLER IMAGE</p>
+            </div>
+            <div className='options'>
+                <div>
+                    <button className="btn li">Candy</button>
+            
+                    <button className="btn li">Coins</button>
+                </div>
+                <div>
+                        <button className="btn li">Hat 1</button>
+
+                        <button className="btn li">Hat 2</button>
+                </div>
+            </div>
         </div>
     )
 }
